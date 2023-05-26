840d3350456aea33e9368a19327f8cfbcac9fe7e
ArchItalia <117321045+ArchItalia@users.noreply.github.com>
2023-05-26 11:17:32 +0100

    Add files via upload

---
+++ 1-parte.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# author Jonathan Sanfilippo
+# script per l'installazione di Arch Linux
+
+#nvme - M2
+#p1="/dev/nvme0n1p1"
+#p2="/dev/nvme0n1p2"
+#p3="/dev/nvme0n1p3"
+
+#vda - Virtual Machine
+#p1="/dev/vda1"
+#p2="/dev/vda2"
+#p3="/dev/vda3"
+
+#sda - SSD
+p1="/dev/sda1"
+p2="/dev/sda2"
+p3="/dev/sda3" 
+
+#Setting - Country, editor, kernel
+country="gb"
+editor="vim"
+kernel="linux"
+
+
+
+#Formattazione delle partizioni - EFI, ROOT, HOME
+mkfs.fat -F32 $p1
+mkfs.btrfs -f $p2 
+mkfs.btrfs -f $p3
+
+#Montaggio e sottovolumi - partizione /home separata
+mount $p2 /mnt           
+btrfs su cr /mnt/@  
+umount /mnt 
+mount $p3 /mnt
+btrfs su cr /mnt/@home      
+umount /mnt                             
+mount -o noatime,commit=120,compress=zstd,space_cache=v2,ssd,subvol=@ $p2 /mnt 
+mkdir -p /mnt/boot
+mount $p1 /mnt/boot 
+mkdir -p /mnt/home
+mount -o noatime,commit=120,compress=zstd,space_cache=v2,ssd,subvol=@home $p3 /mnt/home 
+
+
+
+
+reflector --verbose -c $country --sort rate --save /etc/pacman.d/mirrorlist
+pacstrap /mnt base base-devel $kernel linux-firmware $editor
+genfstab -Up /mnt > /mnt/etc/fstab
+cp 2-parte.sh /mnt/home/
+arch-chroot /mnt 
+
---
+++ 2-parte.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# author Jonathan Sanfilippo
+# installazione parte seconda chroot
+
+
+
+localhost="archlinux"
+user="username"
+rootpw="password"
+userpw="password"
+localegen="en_US.UTF-8 UTF-8"
+localeconf="LANG=en_US.UTF-8"
+km="us"
+localtime="Europe/London"
+p="sda2"
+#p="vda2"
+#p="nvme0n1p2"
+
+
+
+
+ln -sf /usr/share/zoneinfo/$localtime /etc/localtime 
+hwclock --systohc
+echo "$localegen" > /etc/locale.gen
+locale-gen
+echo "$localeconf" >> /etc/locale.conf
+echo "KEYMAP=$km" >> /etc/vconsole.conf  
+echo "$localhost" > /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts 
+echo "::1       localhost" >> /etc/hosts
+echo root:$rootpw | chpasswd
+useradd -m $user
+echo $user:$userpw | chpasswd
+usermod -aG wheel $user
+usermod -aG video $user
+echo "$user ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/$user
+echo "$user ALL=NOPASSWD: /usr/bin/pacman" >> /etc/sudoers.d/$user
+echo "$user ALL=NOPASSWD: /usr/bin/pikaur" >> /etc/sudoers.d/$user
+echo "$user ALL=NOPASSWD: /usr/bin/vim" >> /etc/sudoers.d/$user
+
+
+
+#systemd-boot
+pacman -S efibootmgr
+bootctl --path=/boot install
+echo "default arch-*" >> /boot/loader/loader.conf
+echo "timeout 3" >> /boot/loader/loader.conf
+echo "title Arch Linux
+linux /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=/dev/$p rootflags=subvol=@ rw quiet loglevel=3 rd.system.show_status=auto rd.udev.log_level=3" > /boot/loader/entries/arch.conf
+
+
+#GNOME Minimal gnome installation
+ pacman -S wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils pipewire-pulse firewalld mtools dosfstools gparted reflector acpi cronie git gnome-tweaks gnome-shell nautilus gnome-console gvfs gnome-control-center xdg-user-dirs-gtk gdm xorg evince eog gnome-text-editor gnome-keyring
+ systemctl enable gdm
+
+#Cinnamon
+# pacman -S cinnamon nemo-fileroller gnome-terminal lightdm-gtk-greeter lightdm-gtk-greeter-settings xdg-user-dirs-gtk xorg wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils pipewire-pulse firewalld mtools dosfstools gparted bluez bluez-utils reflector acpi cronie git xed evince eog gnome-calculator gnome-clocks gnome-system-monitor gparted  
+# systemctl enable lightdm
+
+#XFCE
+# pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings epiphany xorg wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils pipewire-pulse firewalld mtools dosfstools gparted bluez bluez-utils reflector acpi cronie pacman-contrib git wget jq gparted 
+# systemctl enable lightdm
+
+#KDE
+# pacman -S sddm plasma ark konsole dolphin xorg wpa_supplicant wireless_tools netctl net-tools iw networkmanager alsa-utils  firewalld mtools dosfstools gparted bluez bluez-utils reflector acpi cronie pacman-contrib git wget jq gparted 
+# systemctl enable sddm  
+
+#servizi
+systemctl enable NetworkManager
+systemctl enable firewalld
+#systemctl enable bluetooth
+systemctl enable cronie
+#systemctl enable reflector.timer
+
+
+rm -r /home/2-parte.sh
+
+
+
+
+
+
+
+
