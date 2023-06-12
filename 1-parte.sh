66e982f4aa38c4fc3d1406e6c0294c93ef5d7ba5
Jonalinux <103053714+jonalinux@users.noreply.github.com>
2023-06-12 23:14:25 +0100

    Update 1-parte.sh

--- 1-parte.sh
+++ 1-parte.sh
@@ -2,6 +2,14 @@
 # author Jonathan Sanfilippo
 # script per l'installazione di Arch Linux
 
+# Colors
+Green='\033[1;32m'
+Blue='\033[1;34m'
+Red='\033[1;31m'
+Yellow='\033[0;33m'
+Color_Off='\033[0m'
+
+
 #nvme - M2
 #p1="/dev/nvme0n1p1"
 #p2="/dev/nvme0n1p2"
@@ -42,12 +50,30 @@ mount $p1 /mnt/boot
 mkdir -p /mnt/home
 mount -o noatime,ssd,space_cache=v2,compress=zstd,discard=async,subvol=@home $p3 /mnt/home 
 
-
-
-
+# reflector
 reflector --verbose -c $country -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+
+#pacstrap
 pacstrap -K /mnt base base-devel $kernel linux-firmware intel-ucode btrfs-progs $editor
+
+#fstab
 genfstab -Up /mnt > /mnt/etc/fstab
-cp 2-parte.sh /mnt/home/
-arch-chroot /mnt 
+
+
+# List of packages to check
+PACKAGES=(base base-devel $kernel linux-firmware intel-ucode btrfs-progs $editor)
+# Get a list of installed packages
+INSTALLED=$(pacman -Qqe)
+
+# Iterate through the list of packages
+for package in "${PACKAGES[@]}"; do
+  if echo "$INSTALLED" | grep -qw "$package"; then
+    echo -e "${Green}$package is installed${Color_Off}"
+    cp 2-parte.sh /mnt/home/
+    arch-chroot /mnt 
+  else
+    echo -e "${Red}$package is NOT installed${Color_Off}"
+  fi
+done
+
 
