07d6a87c118a7ccaef2f6f8554c123ae080eb270
ArchItalia <117321045+ArchItalia@users.noreply.github.com>
2023-06-14 18:23:06 +0100

    Update 1-parte.sh

--- 1-parte.sh
+++ 1-parte.sh
@@ -2,13 +2,17 @@
 # author Jonathan Sanfilippo
 # script per l'installazione di Arch Linux
 
-# Colors
-Green='\033[1;32m'
-Blue='\033[1;34m'
-Red='\033[1;31m'
-Yellow='\033[0;33m'
-Color_Off='\033[0m'
+# settings script 1
 
+country="us" # Mirror region
+age="6" # Age server mirrors
+editor="vim" # choose a editor 
+kernel="linux" # kernel
+FM="linux-firmware" # firmware
+tools="intel-ucode btrfs-progs" # tools
+base="base base-devel" # base packages
+
+# --- nomenclatura --- nomenclature
 
 #nvme - M2
 #p1="/dev/nvme0n1p1"
@@ -25,10 +29,10 @@ Color_Off='\033[0m'
 #p2="/dev/sda2"
 #p3="/dev/sda3" 
 
-#Setting - Country, editor, kernel
-country="us" # us
-editor="vim" # vim - nano 
-kernel="linux"
+
+# end settings -----------------------------------------------
+
+
 
 
 
@@ -45,16 +49,15 @@ mount $p3 /mnt
 btrfs su cr /mnt/@home      
 umount /mnt                             
 mount -o noatime,ssd,space_cache=v2,compress=zstd,discard=async,subvol=@ $p2 /mnt 
-mkdir -p /mnt/boot
+mkdir -p /mnt/{boot,home}
 mount $p1 /mnt/boot 
-mkdir -p /mnt/home
 mount -o noatime,ssd,space_cache=v2,compress=zstd,discard=async,subvol=@home $p3 /mnt/home 
 
 # reflector
-reflector --verbose -c $country -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+reflector --verbose -c $country -a $age --sort rate --save /etc/pacman.d/mirrorlist
 
 #pacstrap
-pacstrap -K /mnt base base-devel $kernel linux-firmware intel-ucode btrfs-progs $editor
+pacstrap -K /mnt $base $FM $kernel $tools $editor
 
 #fstab
 genfstab -Up /mnt > /mnt/etc/fstab
