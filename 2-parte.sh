817657d556708deff69171980c650bc90990e560
ArchItalia <117321045+ArchItalia@users.noreply.github.com>
2023-06-14 23:40:57 +0100

    Update 2-parte.sh

--- 2-parte.sh
+++ 2-parte.sh
@@ -28,7 +28,7 @@ DE="xorg gnome-shell nautilus gnome-console gvfs gnome-control-center xdg-user-d
 DM="gdm" # Display Manager
 Service="gdm NetworkManager firewalld bluetooth cronie reflector" # Service
 
-# root=/dev/XXX decommenta la nomenclatura in uso per systemd-boot IMPORTANTE!
+# [root=/dev/XXX] decommenta la nomenclatura in uso per systemd-boot IMPORTANTE! -- uncomment the nomenclature in use for systemd-boot IMPORTANT!
 
 #p="sda2" 
 #p="vda2"
